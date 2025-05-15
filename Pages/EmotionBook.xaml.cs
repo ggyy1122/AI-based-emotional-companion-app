5e704b7287bb9616d52c4e6d6848f121619ac3b9
Yi Guan <3056399771@qq.com>
2025-05-15 21:50:13 +0800

    首次提交

---
+++ Pages/EmotionBook.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace GameApp.Pages
+{
+    public partial class EmotionBook : Page
+    {
+        public EmotionBook()
+        {
+            InitializeComponent();
+        }
+
+        private void BackToMainPage_Click(object sender, RoutedEventArgs e)
+        {
+            // 返回上一个页面
+            if (this.NavigationService.CanGoBack)
+                this.NavigationService.GoBack();
+            else
+                this.NavigationService.Navigate(new MainPage());
+        }
+    }
+}
---
+++ Pages/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using GameApp.Pages;
+
+namespace GameApp
+{
+    /// <summary>
+    /// MainPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void GoToSecondPage_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new EmotionBook());
+        }
+    }
+}
