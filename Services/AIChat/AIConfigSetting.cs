0ddcff1e19e35303a655e37099432f243f0c6fe3
2edbef4a9b <echofedn@outlook.com>
2025-06-13 19:28:32 +0800

    Change LLM to gpt-4o to avoid ui freezing issue

--- Services/AIChat/AIConfigSetting.cs
+++ Services/AIChat/AIConfigSetting.cs
@@ -2,12 +2,12 @@ namespace GameApp.Services.AIChat
 {
     public static class AIConfigSettings
     {
-        public const string ApiEndpoint = "https://api.deepseek.com/chat/completions";            //"https://openrouter.ai/api/v1/chat/completions";
+        public const string ApiEndpoint = "https://openrouter.ai/api/v1/chat/completions";
 
-        // Replace with your actual API key or retrieve from secure storage
-        public const string ApiKey = "sk-5fcd3143b1b94901b8b8a6b245bb8f0f";//" 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";
+        // Replace with your actual API key.
+        public const string ApiKey = "API_KEY";
 
-        public const string ModelName = "deepseek-chat";// "openai/gpt-4o-mini";
+        public const string ModelName = "openai/gpt-4o-mini";
 
         public const string SystemPrompt = @"
 You are an empathetic AI companion designed to provide emotional support.
